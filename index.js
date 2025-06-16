e28d76bce6f45825c2301e4bcca1b58cba586829
VeejeyPL <wojciech.jurkowski4@gmail.com>
2025-06-16 14:38:42 +0200

    Add header with nav

---
+++ index.js
@@ -0,0 +1,19 @@
+const button = document.querySelector("button");
+const navMobile = document.querySelector(".nav-menu");
+const header = document.querySelector(".header");
+
+button.addEventListener("click", () => {
+  const currentState = button.getAttribute("data-state");
+
+  if (!currentState || currentState === "closed") {
+    button.setAttribute("data-state", "opened");
+    button.setAttribute("aria-expanded", "true");
+    navMobile.classList.add("active");
+    header.classList.add("active");
+  } else {
+    button.setAttribute("data-state", "closed");
+    button.setAttribute("aria-expanded", "false");
+    navMobile.classList.remove("active");
+    header.classList.remove("active");
+  }
+});
