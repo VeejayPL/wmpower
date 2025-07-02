7cfee580927e91763d3d4785ed13b7c9429f535f
VeejeyPL <wojciech.jurkowski4@gmail.com>
2025-07-02 13:09:43 +0200

    Add remaining svgs and animation

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const button = document.querySelector("button");
 const navMobile = document.querySelector(".nav-menu");
 const header = document.querySelector(".header");
+const itemList = document.querySelectorAll(".animation");
+
+const options = { threshold: 0.2 };
+
+const callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+      observer.unobserve(entry.target);
+    }
+  });
+};
+
+const myObserver = new IntersectionObserver(callback, options);
+
+itemList.forEach((item) => {
+  myObserver.observe(item);
+});
 
 button.addEventListener("click", () => {
   const currentState = button.getAttribute("data-state");
